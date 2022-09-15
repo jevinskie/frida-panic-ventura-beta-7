5be53882aa995a02cc485914e7f335107854eded
Jevin Sweval <jevinsweval@gmail.com>
2022-09-15 15:32:43 -0400

    even with KERN_FAILURE

--- frida-panic.c
+++ frida-panic.c
@@ -8,11 +8,11 @@
 kern_return_t my_IOServiceGetMatchingServices(mach_port_t mainPort, CFDictionaryRef matching,
                                               io_iterator_t *existing) {
     assert(existing);
-    *existing = 0x0BAADC0D;
+    // *existing = 0x0BAADC0D;
     printf("IOServiceGetMatchingServices(mainPort: 0x%08x, matching: %p, existing: %p) = "
            "*existing: 0x%08x\n",
            mainPort, matching, existing, *existing);
-    return KERN_SUCCESS;
+    return KERN_FAILURE;
 }
 
 int main(void) {
@@ -23,15 +23,6 @@ int main(void) {
 
     assert(!gum_interceptor_replace(interceptor, IOServiceGetMatchingServices,
                                     my_IOServiceGetMatchingServices, NULL, NULL));
-    // assert(!gum_interceptor_replace(interceptor, IOServiceOpen, my_IOServiceOpen, NULL, NULL));
-    // assert(
-    //     !gum_interceptor_replace(interceptor, IOServiceMatching, my_IOServiceMatching, NULL, NULL));
-    // assert(!gum_interceptor_replace(interceptor, IOObjectRelease, my_IOObjectRelease, NULL, NULL));
-    // assert(!gum_interceptor_replace(interceptor, IOMasterPort, my_IOMasterPort, NULL, NULL));
-    // assert(!gum_interceptor_replace(interceptor, IOIteratorNext, my_IOIteratorNext, NULL, NULL));
-    // assert(!gum_interceptor_replace(interceptor, IOConnectCallMethod, my_IOConnectCallMethod, NULL,
-    //                                 NULL));
-    // assert(!gum_interceptor_replace(interceptor, fopen, my_fopen, NULL, (gpointer *)&orig_fopen));
 
     gum_interceptor_end_transaction(interceptor);
 
